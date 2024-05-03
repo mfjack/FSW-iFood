6b7c4525c4fc8ad77f3ffc42b7b010791e07a72a
mfjack <ferreira.marlon@live.com>
2024-05-03 11:37:54 -0300

    feat: add product list and product item components

---
+++ app/_components/product-item.tsx
@@ -0,0 +1,62 @@
+import { Prisma } from "@prisma/client";
+import Image from "next/image";
+import { calculateProductTotalPrice, formatCurrency } from "../_helpers/price";
+import { ArrowDown } from "lucide-react";
+
+interface ProductItemProps {
+  product: Prisma.ProductGetPayload<{
+    include: {
+      restaurant: {
+        select: {
+          name: true;
+        };
+      };
+    };
+  }>;
+}
+
+const ProductItem = ({ product }: ProductItemProps) => {
+  return (
+    <div className="w-[150px] min-w-[150px] space-y-2">
+      <div className="relative h-[150px] w-full">
+        <Image
+          src={product.imageUrl}
+          alt={product.name}
+          fill
+          className="rounded-lg object-cover shadow-md"
+        />
+
+        {product.discountPercentage && (
+          <div className="absolute left-2 top-2 flex items-center rounded-full bg-primary  px-2 py-[2px] text-white">
+            <ArrowDown size={12} />
+            <span className="text-xs font-semibold ">
+              {product.discountPercentage}%
+            </span>
+          </div>
+        )}
+      </div>
+
+      <div>
+        <h2 className="truncate">{product.name}</h2>
+
+        <div className="flex items-center gap-3">
+          <h3 className="font-semibold">
+            {formatCurrency(calculateProductTotalPrice(product))}
+          </h3>
+
+          {product.discountPercentage > 0 && (
+            <h4 className="text-xs text-muted-foreground line-through">
+              {formatCurrency(Number(product.price))}
+            </h4>
+          )}
+        </div>
+
+        <span className="block text-xs text-muted-foreground">
+          {product.restaurant.name}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default ProductItem;
