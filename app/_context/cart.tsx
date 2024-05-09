1b972cec70bfc8a05709ed56b1ef42edde424d20
mfjack <ferreira.marlon@live.com>
2024-05-09 14:30:41 -0300

    fix: total price with discount

--- app/_context/cart.tsx
+++ app/_context/cart.tsx
@@ -66,11 +66,12 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
     return (
       products.reduce((acc, product) => {
         return acc + calculateProductTotalPrice(product) * product.quantity;
-      }, 0) - Number(products?.[0]?.restaurant?.deliveryFee)
+      }, 0) + Number(products?.[0]?.restaurant?.deliveryFee)
     );
   }, [products]);
 
-  const totalDiscount = subtotalPrice - totalPrice;
+  const totalDiscount =
+    subtotalPrice - totalPrice + Number(products?.[0]?.restaurant?.deliveryFee);
 
   const decrementProductQuantity = (productId: string) => {
     return setProducts((prev) =>
